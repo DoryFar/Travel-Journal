d6e3044bdee010851eb3b017a013a49425f28d45
Dorsa Ershadifar <er.dorsa@gmail.com>
2023-03-16 15:18:15 -0400

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,50 @@
+import tehran from "./images/tehran.jpg";
+import frankfurt from "./images/frankfurt.jpg";
+import paris from "./images/paris.jpg";
+import fiji from "./images/fiji.jpg";
+
+
+const data = [
+    {
+        id: "1",
+        title: "Tehran",
+        location: "Iran",
+        googleMapsUrl: "https://goo.gl/maps/ERzZ5MguRPXDudxd8",
+        startDate: "10 May, 2016",
+        endDate: "25 Aug, 2016",
+        description: "My hometown. Tehran is the capital of Iran, in the north of the country. Its central Golestan Palace complex, with its ornate rooms and marble throne, was the seat of power of the Qajar dynasty.",
+        imageUrl: tehran
+    },
+    {
+        id: "2",
+        title: "Frankfurt",
+        location: "Germany",
+        googleMapsUrl: "https://goo.gl/maps/AKmSDUFn6P34TLit6",
+        startDate: "10 Feb, 2011",
+        endDate: "28 Feb, 2011",
+        description: "Frankfurt, a central German city on the river Main, is a major financial hub that's home to the European Central Bank. It's the birthplace of famed writer Johann Wolfgang von Goethe, whose former home is now the Goethe House Museum.",
+        imageUrl: frankfurt
+    },
+    {
+        id: "3",
+        title: "Paris",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/N3H5YDA2H8UttYMYA",
+        startDate: "12 Jan, 2008",
+        endDate: "24 Jan, 2008",
+        description: "Also known as the Latin Quarter, the 5th arrondissement is home to the Sorbonne university and student-filled cafes. It's also known for its bookshops, including the famed Shakespeare & Company.",
+        imageUrl: paris
+    },
+    {
+        id: "4",
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: fiji
+    }
+]
+
+export default data
