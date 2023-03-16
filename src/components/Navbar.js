4f017d9c95fa1c1cd3be7feaf78ec2398b463e9d
Dorsa Ershadifar <er.dorsa@gmail.com>
2023-03-16 15:18:17 -0400

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://www.freepnglogos.com/uploads/earth-png/world-earth-globe-image-pixabay-9.png" alt="globe" className="logo" />
+            <h1 className="title">My Travel Journal</h1>
+        </nav>
+    )
+}
